bc29aec3c2c6fb3c551fb71c9b41b3690188c09c
perfectimhetsike.igbadumhe@stanbicibtc.com <PerfectImhetsike.Igbadumhe@stanbicibtc.com>
2021-07-25 23:33:35 +0100

    adding mongodb driver

---
+++ Catalogue/Repositories/MongoDBItemsRepository.cs
@@ -0,0 +1,45 @@
+﻿using Catalogue.Entities;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalogue.Repositories
+{
+    public class MongoDBItemsRepository : ITemsRepository
+    {
+        private const string databaseName = "catalog";
+        private const string collectionName = "item";
+        private readonly IMongoCollection<Item> itemsCollection;
+        public MongoDBItemsRepository(IMongoClient mongoClient)
+        {
+            IMongoDatabase database = mongoClient.GetDatabase(databaseName);
+            itemsCollection = database.GetCollection<Item>(collectionName);
+        }
+        public void CreateItem(Item item)
+        {
+            itemsCollection.InsertOne(item);
+        }
+
+        public void DeleteItem(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Item GetItem(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Item> GetItems()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateItem(Item item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
