09271a74dc7db299c4a5ba6412bf6242ef3ccca3
perfectimhetsike.igbadumhe@stanbicibtc.com <PerfectImhetsike.Igbadumhe@stanbicibtc.com>
2021-07-25 01:02:53 +0100

    Entity controller repository and Get

---
+++ Catalogue/Controllers/ItemsController.cs
@@ -0,0 +1,48 @@
+﻿using Catalogue.Entities;
+using Catalogue.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalogue.Controllers
+{
+    [Route("api/items")]
+    [ApiController]
+    public class ItemsController : ControllerBase
+    {
+        private readonly InMemoryItemsRepository _repository;
+        public ItemsController()
+        {
+            _repository = new InMemoryItemsRepository();
+        }
+
+        /// <summary>
+        ///  GET /api/items
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public IEnumerable<Item> GetItems()
+        {
+            var items = _repository.GetItems();
+            return items;
+        }
+
+        /// <summary>
+        /// GET /items/id from query
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet("{id}")]
+        public ActionResult<Item> GetItem(Guid id)
+        {
+            var item = _repository.GetItem(id);
+
+            if (item is null) return NotFound();
+
+            return item;
+        }
+    }
+}
---
+++ Catalogue/Repositories/InMemoryItemsRepository.cs
@@ -0,0 +1,28 @@
+﻿using Catalogue.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalogue.Repositories
+{
+    public class InMemoryItemsRepository
+    {
+        private readonly List<Item> items = new() //older versions ==> new List<Item>() {}
+        {
+            new Item { Id = Guid.NewGuid(), Name = "Portion", Price = 9, CreatedDate = DateTimeOffset.UtcNow },
+            new Item { Id = Guid.NewGuid(), Name = "Iron Sword", Price = 20, CreatedDate = DateTimeOffset.UtcNow },
+            new Item { Id = Guid.NewGuid(), Name = "Bronze Shield", Price = 18, CreatedDate = DateTimeOffset.UtcNow }
+        };
+
+        public IEnumerable<Item> GetItems()
+        {
+            return items;
+        }
+
+        public Item GetItem(Guid id)
+        {
+            return items.Where(item => item.Id == id).SingleOrDefault();
+        }
+    }
+}
